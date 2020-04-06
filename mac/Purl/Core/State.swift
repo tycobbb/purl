916e25eda42499a72709beabdea364fdedb773a0
Ty Cobb <ty.cobb.m@gmail.com>
2020-04-06 18:46:12 -0500

    add state, rename ref file

---
+++ mac/Purl/Core/State.swift
@@ -0,0 +1,33 @@
+final class State<T: Equatable> {
+  typealias Observer = (T) -> Void
+
+  // -- props --
+  var value: T {
+    didSet {
+      if value != oldValue {
+        didChangeValue()
+      }
+    }
+  }
+
+  // -- props/internal
+  private var observers: [Observer] = []
+
+  // -- lifetime --
+  init(_ initial: T) {
+    value = initial
+  }
+
+  // -- queries --
+  func observe(observer: @escaping Observer) {
+    observers.append(observer)
+    observer(value)
+  }
+
+  // -- events --
+  private func didChangeValue() {
+    for observer in observers {
+      observer(value)
+    }
+  }
+}
